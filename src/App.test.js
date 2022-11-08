a6002b60e08c2ba6fdee43d2226273f321a9d811
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    Integrate teammate greeting and prepare version 1.0.1

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the team greeting and logo', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText('Hello World! This is our team project.')).toBeInTheDocument();
+  expect(screen.getByRole('img', { name: 'logo' })).toBeInTheDocument();
 });
